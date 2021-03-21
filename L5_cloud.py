90e7f9735aae7c2f2959b6430dafedd2323ab231
panlu123 <78306711+panlu123@users.noreply.github.com>
2021-03-21 17:44:37 +0800

    L5

---
+++ L5_cloud.py
@@ -0,0 +1,56 @@
+from wordcloud import WordCloud
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+from nltk.tokenize import word_tokenize
+
+#数据导入
+data = pd.read_csv('./Market_Basket_Optimisation.csv',header=None)
+
+trans=[]
+item_count={}
+
+for i in range (data.shape[0]):
+    temp=[]
+    for j in range(data.shape[1]):
+        item=str(data.values[i,j])
+        if item!='nan':
+            temp.append(item)
+            if item not in item_count:
+                item_count[item]=1
+            else:
+                item_count[item]+=1
+    trans.append(temp)
+
+# 去掉停用词
+def remove_stop_words(f):
+	stop_words = ['my']
+	for stop_word in stop_words:
+		f = f.replace(stop_word, '')
+	return f
+
+# 生成词云
+def create_word_cloud(f):
+	print('根据词频，开始生成词云!')
+	f = remove_stop_words(f)
+	cut_text = word_tokenize(f)
+	#print(cut_text)
+	cut_text = " ".join(cut_text)
+	wc = WordCloud(
+		max_words=100,
+		width=2000,
+		height=1200,
+    )
+	wordcloud = wc.generate(cut_text)
+	# 写词云图片
+	wordcloud.to_file("wordcloud.jpg")
+	# 显示词云文件
+	plt.imshow(wordcloud)
+	plt.axis("off")
+	plt.show()
+    
+# 生成词云
+all_word=''.join('%s'%item for item in trans)
+create_word_cloud(all_word)
+print(sorted(item_count.items(),reverse=True)[:10])
